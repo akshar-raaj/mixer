f6bb4b75fbe6efdc6db3a0bf2ffa98630c155e66
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-29 11:53:02 +0530

    Demonstrates moduledoc and doc. Can use markdown and code blocks.

--- lib/hello_system.ex
+++ lib/hello_system.ex
@@ -1,4 +1,17 @@
 defmodule HelloSystem do
+  @moduledoc """
+  Demonstrates `System` module functions and usage.
+  """
+
+  @doc"""
+  Finds the current working directory, a similar functionality to
+  Linux `pwd`.
+
+  ## Examples:
+
+      iex> HelloSystem.current_directory()
+      "/app/hello"
+  """
   def current_directory() do
     System.cwd()
   end
