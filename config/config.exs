85b7a2e7078ee9a08741fecdef628e0ddbc97545
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-02 01:06:07 +0530

    PostgreSQL connectivity. Added dependencies ecto, ecto_sql. Added relevant configuration using 'config'

--- config/config.exs
+++ config/config.exs
@@ -7,3 +7,8 @@ config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
   secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
   region: "us-west-2"
+config :hello, Hello.Repo,
+  database: "throw",
+  username: "postgres",
+  hostname: "postgres-db",
+  password: "postgres"
