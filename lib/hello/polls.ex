bfbc071d4f621a758d629cef83daa1fb52587bad
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-02 16:03:34 +0530

    Added changeset for Poll

--- lib/hello/polls.ex
+++ lib/hello/polls.ex
@@ -6,9 +6,16 @@ defmodule Hello.Poll do
       iex> %Hello.Polls{question: "Who is Dr. Kalam", difficulty: "easy"}
   """
   use Ecto.Schema
+  import Ecto.Changeset
   schema "polls" do
     field :question, :string
     field :difficulty, :string
     has_many :choices, Hello.Choice
+  end
+
+  def changeset(poll, params \\ %{}) do
+    poll
+    |> cast(params, [:question, :difficulty])
+    |> validate_required([:question, :difficulty])
   end
 end
