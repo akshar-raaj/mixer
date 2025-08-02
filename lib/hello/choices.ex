7b4e678b27503cd7dd8985194a0e3466810a33a6
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-02 13:15:47 +0530

    Added changeset/2 to Hello.Choice. We need to import Ecto.Changeset for it.

--- lib/hello/choices.ex
+++ lib/hello/choices.ex
@@ -1,9 +1,16 @@
 defmodule Hello.Choice do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "choices" do
     field :answer, :string
     field :vote_count, :integer
     belongs_to :poll, Hello.Poll
+  end
+
+  def changeset(choice, params \\ %{}) do
+    choice
+    |> cast(params, [:answer, :vote_count])
+    |> validate_required([:answer, :vote_count])
   end
 end
