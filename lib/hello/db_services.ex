68d3e98ba810a866aa67ee93a082ce3a21480b71
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-02 22:43:25 +0530

    Added a service to list polls, used 'preload' keyword list argument to load choices. Generated with Roo.

--- lib/hello/db_services.ex
+++ lib/hello/db_services.ex
@@ -1,4 +1,5 @@
 defmodule Hello.DB.Service do
+  import Ecto.Query, only: [from: 2]
   @moduledoc """
     Services to interact with the database.
   """
@@ -8,6 +9,12 @@ defmodule Hello.DB.Service do
     |> Hello.Poll.changeset(%{question: question, difficulty: difficulty})
     |> Hello.Repo.insert
   end
+
+  def list_polls do
+    query = from p in Hello.Poll, select: p, preload: [:choices]
+    Hello.Repo.all(query)
+  end
+
 
   def create_choice(answer, vote_count \\ 0) do
     %Hello.Choice{}
