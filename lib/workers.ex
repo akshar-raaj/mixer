64fe5acfe2c3c20c5e64acaf56f37ed8241a9722
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-03 01:22:58 +0530

    Added a worker

---
+++ lib/workers.ex
@@ -0,0 +1,8 @@
+defmodule Hello.HelloWorker do
+  use Oban.Worker, queue: :default
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"message" => message}} = _args) do
+    IO.puts "Worker says: " <> message
+  end
+end
