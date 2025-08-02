96be06e16d63b4dce00263c8b5f35615fde80aa4
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-03 00:22:45 +0530

    Refactor. Group configurations for :hello in a single call to config.

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,9 @@
 import Config
 
-config :hello, :author, "Raaj Akshar"
-config :hello, :organisation, :ascend_together
-config :hello, :team, "EPD"
+config :hello,
+  author: "Raaj Akshar",
+  organisation: :ascend_together,
+  team: "EPD"
 config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
   secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
