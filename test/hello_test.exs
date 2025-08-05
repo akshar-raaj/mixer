ec67aac1ce894ca0c955eb206ca38fcf5c4ea84f
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-05 09:34:03 +0530

    Added tests

--- test/hello_test.exs
+++ test/hello_test.exs
@@ -5,4 +5,12 @@ defmodule HelloTest do
   test "greets the world" do
     assert Hello.hello() == :world
   end
+
+  test "greets personaly default" do
+    assert Hello.greet() == "Hello, akshar!"
+  end
+
+  test "greet personally" do
+    assert Hello.greet("prasad") == "Hello, prasad!"
+  end
 end
