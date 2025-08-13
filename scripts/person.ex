334b095935c68a2cdb0287a2ad5d988d1a9a71f2
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-13 13:14:32 +0530

    Make predict_age_income private. Perform validation at the boundary, i.e in predict_income. predict_age_income can then assume that it will always be provided number inputs.

--- scripts/person.ex
+++ scripts/person.ex
@@ -3,10 +3,15 @@ defmodule Person do
     "I am #{name}"
   end
 
-  def predict_income(age, city \\ "Hyderabad") do
+  def predict_income(age, city \\ "Hyderabad") when is_number(age)  and is_binary(city) do
     {:ok, age_income} = predict_age_income(age)
     city_income = predict_city_income(city)
     age_income + city_income
+  end
+
+  def predict_income(age, city) do
+    IO.puts "Invalid input. Age must be a number and City must be a string. Provided values, age: #{age} and city: #{city}"
+    {:error, :invalid}
   end
 
   def predict_city_income(city) when is_binary(city) do
@@ -19,18 +24,18 @@ defmodule Person do
     end
   end
 
-  def predict_age_income(age) when is_number(age) and age <= 100 and age > 20 do
+  defp predict_age_income(age) when age <= 100 and age > 20 do
     case age do
       greater_than_50 when greater_than_50 > 50 -> {:ok, 1000}
       less_equal_50 when less_equal_50 <= 50 -> {:ok, 700}
     end
   end
 
-  def predict_age_income(age) when is_number(age) and age < 20 do
+  defp predict_age_income(age) when age < 20 do
     {:error, :minor}
   end
 
-  def predict_age_income(age) when is_number(age) and age > 100 do
+  defp predict_age_income(age) when age > 100 do
     {:error, :too_old}
   end
 end
