62e078917210b8581a33be413ae357b231e3f15e
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-13 12:36:08 +0530

    Multiple function clauses with guards. Idiomatic Elixir to have conditions in the function head rather than function body.

---
+++ scripts/person.ex
@@ -0,0 +1,36 @@
+defmodule Person do
+  def say(name) do
+    "I am #{name}"
+  end
+
+  def predict_income(age, city \\ "Hyderabad") do
+    {:ok, age_income} = predict_age_income(age)
+    city_income = predict_city_income(city)
+    age_income + city_income
+  end
+
+  def predict_city_income(city) when is_binary(city) do
+    case city do
+      "Hyderabad" -> 1000
+      "Bangalore" -> 800
+      "Delhi" -> 600
+      "Mumbai" -> 1200
+      _ -> :invalid
+    end
+  end
+
+  def predict_age_income(age) when is_number(age) and age <= 100 and age > 20 do
+    case age do
+      greater_than_50 when greater_than_50 > 50 -> {:ok, 1000}
+      less_equal_50 when less_equal_50 <= 50 -> {:ok, 700}
+    end
+  end
+
+  def predict_age_income(age) when is_number(age) and age < 20 do
+    {:error, :minor}
+  end
+
+  def predict_age_income(age) when is_number(age) and age > 100 do
+    {:error, :too_old}
+  end
+end
