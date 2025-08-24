85db3b81ad7ba060eaf112a3616c35d86625967f
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-24 22:48:27 +0530

    Worker

--- lib/workers.ex
+++ lib/workers.ex
@@ -1,8 +1,13 @@
 defmodule Hello.HelloWorker do
   use Oban.Worker, queue: :default
+  alias Hello.Repo
+  alias Hello.Poll
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"message" => message}} = _args) do
+    polls = Repo.all Poll
+    first_poll = Poll |> Repo.all |> Enum.at(0)
     IO.puts("Worker says: " <> message)
+    IO.puts("Poll author: #{first_poll.author}")
   end
 end
