fd438af81607a7a471be2b4a2617acdb0afa2521
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-08-31 12:23:36 +0530

    Configure test database

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,8 @@
 import Config
+
+config :hello, Hello.Repo,
+  hostname: "postgres-db",
+  username: "postgres",
+  password: "postgres",
+  database: "throw_test",
+  pool: Ecto.Adapters.SQL.Sandbox
